dbf049aa75a613a115cf71d086f28314438d15b7
fortune.akinremi <fortuneakinremi@gmail.com>
2024-12-29 19:41:04 -0500

    Fixed the tailwind issues

--- app/page.js
+++ app/page.js
@@ -42,48 +42,56 @@ export default function Generate() {
 
 
   return (
-    <div className="max-w-md mx-auto px-4">
-      {/* Generate Flashcards: */}
-      <div className="my-4">
-        <h1 className="text-2xl font-bold mb-4">
-          Generate Flashcards
-        </h1>
-        {/* Get Text to generate flashcards on: */}
-        <textarea
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text"
-          className="w-full p-3 border rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          rows={4}
-        />
-        <button
-          onClick={handleSubmit}
-          className="w-full bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600"
-        >
-          Generate Flashcards
-        </button>
-      </div>
-      
-      
-      {/* Dsiplay the Flashcards gotten from the API: */}
-      {flashcards.length > 0 && (
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2">
-            Generated Flashcards
-          </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {flashcards.map((flashcard, index) => (
-              <div key={index} className="bg-white shadow-md rounded-lg p-4">
-                <h3 className="text-lg font-semibold">Front:</h3>
-                <p>{flashcard.front}</p>
-                <h3 className="text-lg font-semibold mt-4">Back:</h3>
-                <p>{flashcard.back}</p>
-              </div>
-            ))}
-          </div>
+    // <div className="max-w-md mx-auto px-4">
+    <div>
+      <div className="w-full h-screen min-h-full flex flex-col items-center justify-center">
+        {/* Header: */}
+        <div className="w-full items-center pt-10 pb-40">
+          <h1 className="text-2xl text-center font-bold">Flashcards</h1>
         </div>
-      )}
 
+        {/* Generate Flashcards: */}
+        <div className="h-full w-1/4 flex flex-col">
+          <h1 className="text-base mb-4">
+            Generate the flashcards:
+          </h1>
+          {/* Get Text to generate flashcards on: */}
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text"
+            className="w-full p-3 text-sm border rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            rows={4}
+          />
+          <button
+            onClick={handleSubmit}
+            className="w-full bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600"
+          >
+            Generate
+          </button>
+        </div>
+        
+        
+        {/* Dsiplay the Flashcards gotten from the API: */}
+        {flashcards.length > 0 && (
+          <div className="mt-4">
+            <h2 className="text-xl font-semibold mb-2">
+              Generated Flashcards
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {flashcards.map((flashcard, index) => (
+                <div key={index} className="bg-white shadow-md rounded-lg p-4">
+                  <h3 className="text-lg font-semibold">Front:</h3>
+                  <p>{flashcard.front}</p>
+                  <h3 className="text-lg font-semibold mt-4">Back:</h3>
+                  <p>{flashcard.back}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+      </div>
     </div>
   )
 }
